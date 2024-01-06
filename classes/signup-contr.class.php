b29c4fd8a77b051cd4f0101d5e0b087f3fada4e0
TapeMate <admin@tapemate.onmicrosoft.com>
2024-01-06 16:16:33 +0100

    embedded rest of error handling and validation on signup process.

--- classes/signup-contr.class.php
+++ classes/signup-contr.class.php
@@ -18,15 +18,32 @@ class SignupContr extends Signup
     public function signupUser()
     {
         if ($this->emptyInput() == false) {
-            // redirection must be done in a different way!!!
-            // header("location: ../index.php?error=emptyinput");
-            // exit();
-
             // echo error message to frontend
             // exit rest of the script
             echo json_encode(["error" => "empty Input, please fill out all fields!"]);
-            exit(); // Terminate script execution
+            exit();
         }
+
+        if ($this->invalidUid() == false) {
+            echo json_encode(["error" => "Invalid Username taken!"]);
+            exit();
+        }
+
+        if ($this->invalidEmail() == false) {
+            echo json_encode(["error" => "Invalid Email Adress!"]);
+            exit();
+        }
+
+        if ($this->pwdMatch() == false) {
+            echo json_encode(["error" => "Passwords do not match!"]);
+            exit();
+        }
+
+        if ($this->uidTaken() == false) {
+            echo json_encode(["error" => "Username or EMail allready registered!"]);
+            exit();
+        }
+
         parent::setUser($this->uid, $this->pwd, $this->email);
         return ["message" => "Signup successful!"];
     }
@@ -35,6 +52,50 @@ class SignupContr extends Signup
     {
         $result = null;
         if (empty($this->uid) || empty($this->pwd) || empty($this->pwdRepeat) || empty($this->email)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function invalidUid()
+    {
+        $result = null;
+        if (!preg_match("/^[a-zA-Z0-9]*$/", $this->uid)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function invalidEmail()
+    {
+        $result = null;
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function pwdMatch()
+    {
+        $result = null;
+        if ($this->pwd !== $this->pwdRepeat) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+        return $result;
+    }
+
+    private function uidTaken()
+    {
+        $result = null;
+        if (!parent::checkUser($this->uid, $this->email)) {
             $result = false;
         } else {
             $result = true;
